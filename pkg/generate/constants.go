41fe0b3f6b598e8e1562205e4f01b4a1aaf1762c
Federico Di Pierro <nierro92@gmail.com>
2023-10-19 12:02:56 +0200

    chore(pkg,cmd): updated urlArchFmt to point to github pages urls.
    Moreover, updated driverkit to v0.15.2 and dropped LastRunDistro support.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/generate/constants.go
+++ pkg/generate/constants.go
@@ -15,6 +15,5 @@ limitations under the License.
 package generate
 
 const (
-	urlArchFmt    = "https://raw.githubusercontent.com/falcosecurity/kernel-crawler/kernels/%s/list.json"
-	urlLastDistro = "https://raw.githubusercontent.com/falcosecurity/kernel-crawler/kernels/last_run_distro.txt"
+	urlArchFmt = "https://falcosecurity.github.io/kernel-crawler/%s/list.json"
 )
